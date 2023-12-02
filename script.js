fe9e3871fdfc33e78efcf1a8a664fa7eaf8ef92d
Nail499 <146083785+Nail499@users.noreply.github.com>
2023-12-02 09:43:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 let main = document.getElementById("main");
 
-for(let i=0; i<=20; i++){
+for(let i=1; i<=20; i++){
     let div = document.createElement("div");
     div.classList.add("colordiv");
     
-    if(i%2 === 0){
+    if(i%2 == 0){
     div.style.background="red";
-}else{
+}else if(i%2 !=0{
     div.style.background= "yellow";
 }
-if(i%5 === 0){
+if(i%5 == 0){
     div.style.background= "blue"
 }
 main.appendChild(div);
